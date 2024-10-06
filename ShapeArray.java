8feab7cf877f3448705437a7f40782adfd1d7d3d
Tybo323 <butoractyler@gmail.com>
2024-10-06 15:27:34 -0700

    Add files via upload

---
+++ ShapeArray.java
@@ -0,0 +1,15 @@
+package cta4Shapes;
+
+public class ShapeArray {
+    public static void main(String[] args) {
+        Shape[] shapeArray = new Shape[3];
+        shapeArray[0] = (Shape) new Sphere(5);
+        shapeArray[1] = new Cylinder(3, 7);
+        shapeArray[2] = new Cone(4, 6);
+
+        for (Shape shape : shapeArray) {
+            System.out.println(shape);
+        }
+    }
+}
+
